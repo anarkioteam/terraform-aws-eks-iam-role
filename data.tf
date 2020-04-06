4f9ba031620963fbe461dad8311549b3e5252963
Nick Pellant <nick@nickpellant.com>
2020-04-06 20:17:30 +0100

    Fix identity issuer data usage

--- data.tf
+++ data.tf
@@ -1,10 +1,13 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_eks_cluster" "this" {
   name = var.eks_cluster_name
 }
 
 locals {
-  eks_cluster_identity_oidc_issuer_arn = data.aws_eks_cluster.this.identity.0.oidc.0.arn
-  eks_cluster_identity_oidc_issuer     = data.aws_eks_cluster.this.identity.0.oidc.0.issuer
+  eks_cluster_identity_issuer_url = replace(data.aws_eks_cluster.this.identity.0.oidc.0.issuer, "https://", "")
+
+  aws_account_id = data.aws_caller_identity.current.account_id
 }
 
 data "aws_iam_policy_document" "assume_role_with_oidc" {
@@ -17,7 +20,7 @@ data "aws_iam_policy_document" "assume_role_with_oidc" {
       type = "Federated"
 
       identifiers = [
-        local.eks_cluster_identity_oidc_issuer_arn
+        "arn:aws:iam::${local.aws_account_id}:oidc-provider/${local.eks_cluster_identity_issuer_url}"
       ]
     }
 
@@ -25,7 +28,7 @@ data "aws_iam_policy_document" "assume_role_with_oidc" {
       for_each = var.oidc_fully_qualified_subjects
       content {
         test     = "StringEquals"
-        variable = "${replace(local.eks_cluster_identity_oidc_issuer, "https://", "")}:sub"
+        variable = "${local.eks_cluster_identity_issuer_url}:sub"
         values   = [condition.value]
       }
     }
